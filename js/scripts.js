145d1237bb272d5e60877ef7d138e8f34f882a41
AJ0094 <aj@Arnoldos-MacBook-Air.local>
2017-04-03 07:28:11 -0700

    Added to CSS, Index, and added Sidr
    -Added locations and info to index
    -Added Changes to CSS
    -Added Sidr plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,46 @@
 // YOUR SCRIPTS GO HERE
 
+// call Sidr Plugin. Toggle by Default
+$('#sidr-show-hide').sidr({
+	speed:500
+});
+	
+// Close from inside
+$('#close').sidr({
+  method: 'toggle',
+	timimg:'ease-out',
+	speed:500
+});
+
+$('section').sidr({
+	method: 'close',
+	timimg:'ease-out',
+	speed:500
+});
+
+// Get and store Geo Location lat/long coordinates
+if ('geolocation' in navigator) {
+
+   $('.geo').show(); 
+
+} else {
+  
+  $('.geo').hide();
+  $('.geo').prepend('<p>Geolocation Not Supported</p>');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 $.simpleWeather({
   location: 54444,
   
